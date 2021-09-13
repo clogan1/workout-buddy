6a6e80ba875fa4f6d949ef49eb0445ac4c216652
sam <surickj@gmail.com>
2021-09-13 14:05:47 -0700

    first react channge

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,6 +1,24 @@
 import '../App.css';
+import Login from "./Login"
+import {
+  Switch,Route} from "react-router-dom";
+import {useEffect,useState} from "react";
 
 function App() {
+
+ const [user, setUser] = useState(null) 
+
+  useEffect(() => {
+    fetch("/me")
+   .then(res => {
+     if (res.ok) {
+       res.json().then(user =>setUser(user))
+     }
+   })
+  }, [])
+
+  if(!user) return <Login/> 
+
   return (
     <div className="App">
       <h1>Workout Buddy</h1>
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router} from "react-router-dom";
 import './index.css';
 import App from './Components/App'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
 
