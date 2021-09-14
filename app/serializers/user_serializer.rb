836ded6fc452bdc51c5fa30f5166365f9e45a374
Joshua <j.blengino@yahoo.com>
2021-09-14 10:51:07 -0700

    full CRUD on workout_logs/controller and added user stats to user/serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,15 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :password_digest, :avatar_url, :weekly_goal
+  attributes :id, :username, :password_digest, :avatar_url, :weekly_goal, :total_workouts_completed, :weekly_goal_tracker
+
+  has_many :workout_logs
+  has_many :workouts
+
+  def total_workouts_completed
+    self.object.workout_logs.where(is_completed: true).count
+  end
+
+  def weekly_goal_tracker
+    "#{total_workouts_completed} / #{self.weekly_goal}"
+  end
+  
 end
