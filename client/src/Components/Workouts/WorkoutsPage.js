22d8336eb35385e3aefb765063ee6e7294329574
sam <surickj@gmail.com>
2021-09-14 20:45:14 -0700

    workout_page updated

--- client/src/Components/Workouts/WorkoutsPage.js
+++ client/src/Components/Workouts/WorkoutsPage.js
@@ -3,7 +3,10 @@ import React from 'react'
 function WorkoutsPage() {
     return (
         <div>
-            Workouts Page
+           <div className = "WORKOUT-TILES"> Upper Body</div>
+           <div className = "WORKOUT-TILES"> Lower Body</div>
+           <div className = "WORKOUT-TILES"> Core </div>
+           <div className = "WORKOUT-TILES"> Cardio </div>
         </div>
     )
 }
